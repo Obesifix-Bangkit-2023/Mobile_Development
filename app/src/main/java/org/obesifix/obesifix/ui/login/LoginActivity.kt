1352f6a46505678b8003baa126dfb75930964efe
Muhammad Ilham El Hakim <m.ilhamhakim@gmail.com>
2023-05-20 16:28:46 +0700

    adding poppin font

---
+++ app/src/main/java/org/obesifix/obesifix/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.obesifix.obesifix.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.obesifix.obesifix.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/org/obesifix/obesifix/ui/login/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package org.obesifix.obesifix.ui.login
+
+class LoginViewModel {
+}
