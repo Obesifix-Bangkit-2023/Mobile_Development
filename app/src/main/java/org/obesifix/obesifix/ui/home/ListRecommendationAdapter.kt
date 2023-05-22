f46d3472d5b9aaec06c4aa95ff2d93cf5c8fcf16
Muhammad Ilham El Hakim <m.ilhamhakim@gmail.com>
2023-05-22 21:46:08 +0700

    home fragment layout without adapter finish

---
+++ app/src/main/java/org/obesifix/obesifix/ui/home/ListRecommendationAdapter.kt
@@ -0,0 +1,5 @@
+package org.obesifix.obesifix.ui.home
+
+class ListRecommendationAdapter {
+    // to connect between fragment home recycler view that can move to detail activity and item_col_home_recommendation
+}
