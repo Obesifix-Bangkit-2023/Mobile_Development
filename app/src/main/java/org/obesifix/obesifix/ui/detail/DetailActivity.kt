b3d097b16a783a6d512f670db6c06747786485c5
Muhammad Ilham El Hakim <m.ilhamhakim@gmail.com>
2023-05-23 18:41:48 +0700

    Detail suggestion/recommendation layout finish

---
+++ app/src/main/java/org/obesifix/obesifix/ui/detail/DetailActivity.kt
@@ -0,0 +1,12 @@
+package org.obesifix.obesifix.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.obesifix.obesifix.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
